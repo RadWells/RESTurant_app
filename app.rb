bcfb61aaa8ae528411b9f1bcceb6ba952ff76f54
RadWells <scwells@icloud.com>
2014-10-14 20:29:27 -0400

    Added /parties route function

--- app.rb
+++ app.rb
@@ -62,6 +62,7 @@ end
 
 
 #GET | / | Displays links to navigate the application (including links to each current parties)
+
 #+GET | /foods | Display a list of food items available
 #+GET | /foods/:id | Display a single food item and a list of all the parties that included it
 #+GET | /foods/new | Display a form for a new food item
@@ -69,13 +70,32 @@ end
 #+GET | /foods/:id/edit | Display a form to edit a food item
 #+PATCH | /foods/:id | Updates a food item
 #+DELETE | /foods/:id | Deletes a food item
+
+
 #+GET | /parties | Display a list of all parties
+get '/parties' do
+  @patrons = Party.all
+  erb :'party/index'
+end
+
 #+GET | /parties/:id | Display a single party and options for adding a food item to the party
+
+
 #+GET | /parties/new | Display a form for a new party
+
+
 #+POST | /parties | Creates a new party
+
+
 #+GET | /parties/:id/edit | Display a form for to edit a party's details
+
+
 #+PATCH | /parties/:id | Updates a party's details
+
+
 #+DELETE | /parties/:id | Delete a party
+
+
 #+POST | /orders | Creates a new order
 #+PATCH | /orders/:id | Change item to no-charge
 #+DELETE | /orders | Removes an order
